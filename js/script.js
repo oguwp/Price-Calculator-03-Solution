75951faef6bc678072035cc9748f0675e3c5aa63
Ogu <56625756+therangerwp@users.noreply.github.com>
2020-02-03 14:53:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let price = 5;
+let quantity = 6;
+let total = price + quantity;
+document.getElementById("test").innerHTML =
+"Den totale pris er: " + total;
+
+console.log(price);
+console.log(quantity);
+console.log(total);
